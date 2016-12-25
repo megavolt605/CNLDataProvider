ec9ca07f4e7b5b4f24aef322110c6caea206bde2
Igor Smirnov <dev_ios@ronin.pro>
2016-12-25 20:58:59 +0300

    remove pods from repo

--- Pods/Target Support Files/CNLFoundationTools/CNLFoundationTools-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CNLFoundationTools : NSObject
-@end
-@implementation PodsDummy_CNLFoundationTools
-@end
--- Pods/Target Support Files/Pods-CNLDataProvider/Pods-CNLDataProvider-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CNLDataProvider : NSObject
-@end
-@implementation PodsDummy_Pods_CNLDataProvider
-@end
